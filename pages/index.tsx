a5aabb8742f4d822eee48acd3e45f7a4c8812cfb
fnalviar <nikka_alviar@outlook.com>
2023-06-25 16:54:41 -0400

    completed header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
+import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import { NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div
+      className="relative h-screen bg-gradient-to-b from-gray-900/10
+    to-[#010511] lg:h-[140vh]"
+    >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="favicon.ico" />
       </Head>
       <Header />
-      <main>
 
+      <main>
+        <Banner />
       </main>
     </div>
   );
