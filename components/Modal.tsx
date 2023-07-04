91649d16518923d3a3876e9b838e89d4c527c351
fnalviar <nikka_alviar@outlook.com>
2023-07-04 01:04:20 -0400

    added Recoil

---
+++ components/Modal.tsx
@@ -0,0 +1,18 @@
+import { modalState } from "@/atoms/modalAtom";
+import MuiModal from "@mui/material/Modal";
+import { useRecoilState } from "recoil";
+
+function Modal() {
+  const [showModal, setShowModal] = useRecoilState(modalState);
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
+  return (
+    <MuiModal open={showModal} onClose={handleClose}>
+      <>Modal</>
+    </MuiModal>
+  );
+}
+export default Modal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import { AuthProvider } from '../hooks/useAuth'
+import { AuthProvider } from "../hooks/useAuth";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return;
-
-  // HOC - Higher Order Components
-  <AuthProvider>
-    <Component {...pageProps} />
-  </AuthProvider>;
+  return (
+    // HOC - Higher Order Components
+    <RecoilRoot>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </RecoilRoot>
+  );
 }
